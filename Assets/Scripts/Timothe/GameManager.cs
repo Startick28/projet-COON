fa1bbcdc6344d2abded1fad29f17d7be000ed668
Timothé Louzier <louziert@gmail.com>
2021-05-04 00:00:17 +0200

    First visuals

---
+++ Assets/Scripts/Timothe/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    public static GameManager instance;
+
+    public void Awake()
+    {
+        if(instance)
+        {
+            Debug.Log("Il y a déjà une instance de GameManager " + name);
+            Destroy(this);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        Application.targetFrameRate = 60;
+
+
+    }
+}
---
+++ Assets/Scripts/Timothe/SongManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongManager : MonoBehaviour
+{
+    
+    public AudioSource musicSource;
+
+    public float bpm = 120f;
+    public float firstBeatOffset = 0f;
+    public float bps = 2f;
+    public float sbp = 0.5f;
+    public float dspSongTime; //How many seconds have passed since the song started 
+    public float songPosition; //Current song position, in seconds 
+    public float songPositionInBeats; //Current song position, in beats 
+
+   
+    void Start()
+    {
+        bps = bpm / 60f;
+        sbp = 60f/ bpm;
+        dspSongTime = (float) AudioSettings.dspTime;
+    }
+
+
+    void Update()
+    {
+        //determine how many seconds since the song started 
+        songPosition = (float) (AudioSettings.dspTime - dspSongTime - firstBeatOffset); 
+        //determine how many beats since the song started 
+        songPositionInBeats = songPosition * bps; 
+    }
+}

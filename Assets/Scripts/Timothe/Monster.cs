728e51617618821c697bdcdff4f1da980d98cc56
lybryan <bryaan.ly@gmail.com>
2021-05-08 20:36:21 +0200

    Scripts Monster Factory

---
+++ Assets/Scripts/Timothe/Monster.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Monster : MonoBehaviour
+{
+    private Rigidbody rigi;
+    private new Renderer renderer;
+
+[SerializeField]
+private float speed;
+// Use this for initialization
+void Awake()
+{
+    rigi = GetComponent<Rigidbody>();
+    renderer = GetComponent<Renderer>();
+}
+
+private void OnEnable()
+{
+    renderer.material.color = Random.ColorHSV();
+}
+
+private void FixedUpdate()
+{
+    rigi.velocity = transform.up * speed;
+}
+
+private void OnTriggerEnter(Collider other)
+{
+    MonsterFactory.EndMonster(this);
+}
+}
---
+++ Assets/Scripts/Timothe/MonsterFactory.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterFactory : MonoBehaviour
+{
+
+	public static MonsterFactory _instance;
+
+	[SerializeField] private Monster originalMonster;
+
+	private static Queue<Monster> deadMonsters = new Queue<Monster>();
+
+	void Awake()
+	{
+
+		if (_instance == null)
+		{
+
+			_instance = this;
+			DontDestroyOnLoad(this.gameObject);
+
+			//Rest of your Awake code
+
+		}
+		else
+		{
+			Destroy(this);
+		}
+	}
+
+	public GameObject GetMonster()
+	{
+
+		Monster monster;
+
+		if (deadMonsters.Count > 0)
+		{
+			monster = deadMonsters.Dequeue();
+			monster.gameObject.SetActive(true);
+		}
+		else
+		{
+			monster = Instantiate(originalMonster);
+		}
+		return monster.gameObject;
+	}
+
+	public static void EndMonster(Monster monster)
+	{
+		monster.gameObject.SetActive(false);
+		deadMonsters.Enqueue(monster);
+	}
+
+}
